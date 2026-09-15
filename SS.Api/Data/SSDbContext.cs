410cd9c0daac6b427415359af0139cb47c32abb5
Piotr <piaskowski1993@users.noreply.github.com>
2026-09-15 10:58:21 +0200

    initial EF Core migration

--- SS.Api/Data/SSDbContext.cs
+++ SS.Api/Data/SSDbContext.cs
@@ -7,4 +7,8 @@ public class SSDbContext : DbContext
     public DbSet<User> Users { get; set;}
     public SSDbContext(DbContextOptions<SSDbContext> options) : base(options)
     {}
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>().HasKey(u => u.Id);
+    }
 }
